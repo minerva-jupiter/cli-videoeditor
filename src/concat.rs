c4919ac01719f613a74f6c02e9858a0693afd0a3
Minerva_juppiter <94231606+minerva-jupiter@users.noreply.github.com>
2025-05-19 15:30:46 +0900

    Update concat.rs

--- src/concat.rs
+++ src/concat.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::Write;
 
+mod main
+
 const FILE_LIST: &str = "file_list.txt";
 
 // concat mode master
@@ -16,17 +18,15 @@ pub fn add_list(file_path: Vec<String>) {
 }
 
 //execute concat
-pub fn exec_concat(input_files: Vec<String>,output_file: String,args: Vec<String>) {
-    let output = std::process::Command::new("ffmpeg")
-        .arg("-i")
-        .arg(input_files)
-        .arg(output_file)
-        .arg(&args)
-        .output()
-        .expect("Faild to execute ffmpeg");
-    if output.status.success() {
-        println!("FFmpeg Concat executed successfully");
-    } else {
-        eprintln!("FFmpeg Concat execution failed");
-    }
+pub fn exec_concat() {
+    //add -f concat option
+    args :Vec<String> = [];
+    args.append("-f concat");
+
+    //decide input_files output_file
+    const input_files :Vec<String> = main::ask_inputs();
+    const output_file :&str = main::ask_output();
+
+    //execute ffmpeg with some options
+    main::exec_ffmpeg(input_files,output_file,args);
 }
